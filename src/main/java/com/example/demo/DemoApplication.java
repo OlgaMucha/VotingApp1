6dc7d2941093bc54c13f94de718cec29947d975d
Olga Oost <=>
2023-04-16 19:15:27 +0200

    Create classes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,9 @@
-package com.example.demo;
+package votes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
